5260c6506e6a0334a2f7120009fcb24470640d01
Beau <beaulazear@gmail.com>
2023-11-22 15:34:10 -0500

    fixed margin issues on contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,7 +23,6 @@ const StyledContainer = styled.div`
 const StyledColumn = styled.div`
     flex: 1;
     padding: 20px;
-    margin: 10px;
     border: 1px solid #ddd;
     border-radius: 10px;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
